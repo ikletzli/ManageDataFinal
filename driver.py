fddb85b4e44b92d4a18755d1d97dd9500af87dce
Isaac Kletzli <isaackletzli@gmail.com>
2024-03-20 18:54:09 -0600

    read in data from files

--- driver.py
+++ driver.py
@@ -1,7 +1,8 @@
 import pandas as pd
+import numpy as np
+import re
 
-
-# Data Loading Methods =================================================================================================
+# # Data Loading Methods =================================================================================================
 
 def LoadData():
     author_data = LoadAuthors()
@@ -9,10 +10,30 @@ def LoadData():
 
     return author_data, address_data
 
-
 def LoadAuthors():
-    return set()
+    address_df = pd.read_csv('data/address.csv')
+    address_df = address_df[['EIN', 'Street Address 1']]
 
+    address_df = address_df.drop(address_df[address_df['Street Address 1'].isnull()].index)
+    address_df = address_df.drop(address_df[address_df['EIN'].isnull()].index)
+
+    address_df['EIN'] = address_df['EIN'].astype('string')
+    address_df["EIN"] = address_df["EIN"].map(lambda x: re.sub(r'\D', '', x))
+    address_df['EIN'] = address_df['EIN'].astype('int64')
+
+    address_df = address_df.rename(columns={'EIN': 'ein', 'Street Address 1': 'address'})
+
+    return address_df
 
 def LoadAddresses():
-    return set()
+    book_df = pd.read_csv('data/book.txt', sep="\t", header=None, names=['publisher', 'isbn', 'title', 'author'])
+    book_df = book_df.drop('publisher', axis=1)
+    book_df = book_df.drop('title', axis=1)
+
+    return book_df
+
+if __name__ == "__main__":
+    author_data, address_data = LoadData()
+
+    print(author_data)
+    print(address_data)
