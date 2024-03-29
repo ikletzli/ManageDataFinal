dabedc635fc34db5f7e6c06680dc57eb533db69a
Isaac Kletzli <isaackletzli@gmail.com>
2024-03-29 16:27:59 -0600

    Implemented algorithm 8 through line 11

--- driver.py
+++ driver.py
@@ -23,14 +23,16 @@ def LoadAddresses():
 
     address_df = address_df.rename(columns={'EIN': 'ein', 'Street Address 1': 'address'})
 
+    address_df = address_df.fillna('')
     address_df = address_df.groupby('ein')['address'].agg(set= lambda x: set(x))
-
     return address_df
 
 def LoadAuthors():
     book_df = pd.read_csv('data/book.txt', sep="\t", header=None, names=['publisher', 'isbn', 'title', 'author'])
     book_df = book_df.drop('publisher', axis=1)
     book_df = book_df.drop('title', axis=1)
+
+    book_df = book_df.fillna('')
 
     book_df = book_df.groupby('isbn')['author'].agg(set= lambda x: set(x))
 
@@ -56,26 +58,62 @@ def GeneratingCandidateReplacements(author_data, address_data):
     return author_candidates, address_candidates
 
 def BuildTransformationGraphs(author_candidates, address_candidates):
-    author_candidates, address_candidates
-
     for candidate in author_candidates:
         pre_defined_regex = ["[A-Z]+", "[a-z]+", "\s+", "[0-9]+"]
         matches = {"[A-Z]+": [], "[a-z]+": [], "\s+": [], "[0-9]+": []}
         for regex in pre_defined_regex:
-            match = ""
-            match_pos = 0
-            while match != None:
-                match = re.search(regex, candidate[0][match_pos:])
-                if match != None:
-                    match_pos = match.start() + 1
-                    match_list = matches[regex]
-                    match_list.append(match)
-                    matches[regex] = match_list
+            for match in re.finditer(regex, candidate[0]):
+                match_list = matches[regex]
+                match_list.append(match)
+                matches[regex] = match_list
+        
+        P = {}
+        for regex, match_list in matches.items():
+            for i in range(len(match_list)):
+                match = match_list[i]
 
+                x_str_one = "match" + regex + str(i+1) + "B"
+                x_str_two = "match" + regex + str(i-len(match_list)) + "B"
+
+                y_str_one = "match" + regex + str(i+1) + "E"
+                y_str_two = "match" + regex + str(i-len(match_list)) + "E"
+
+                if match.start() not in P:
+                    P[match.start()] = [x_str_one, x_str_two]
+                else:
+                    labels = P[match.start()] 
+                    labels.append(x_str_one)
+                    labels.append(x_str_two)
+                    P[match.start()] = labels
+
+                if match.end() not in P:
+                    P[match.end()] = [y_str_one, y_str_two]
+                else:
+                    labels = P[match.end()] 
+                    labels.append(y_str_one)
+                    labels.append(y_str_two)
+                    P[match.end()] = labels
+
+        for i in range(len(candidate[0]) + 1):
+            k = i+1
+            str_one = "const" + str(k)
+            str_two = "const" + str(k - len(candidate[0]) - 2)
+
+            if k not in P:
+                P[k] = [str_one, str_two]
+            else:
+                labels = P[k] 
+                labels.append(str_one)
+                labels.append(str_two)
+                P[k] = labels
+        
 if __name__ == "__main__":
     author_data, address_data = LoadData()
 
     author_candidates, address_candidates = GeneratingCandidateReplacements(author_data, address_data)
 
-    print(author_data)
-    print(address_data)
+    BuildTransformationGraphs(author_candidates, address_candidates)
+
+
+    # print(author_data)
+    # print(address_data)
